16b25dd0348c70640c5611243ca5af1a175a2eab
mottox2 <y.mottox2@gmail.com>
2019-11-19 04:24:23 +0900

    Divide functions

--- src/listeners/res_cem_delete.ts
+++ src/listeners/res_cem_delete.ts
@@ -1,36 +1,39 @@
 import { app } from '../initializers/bolt'
 import { firestore } from '../initializers/firebase'
-import { Message } from '../types/slack'
+import { Message, Project } from '../types/slack'
+
+const deleteProject = async ({ view, body, context }): Promise<Message> => {
+  const payload = view.state.values
+  const user = body.user.id
+  const metadata = body.view.private_metadata
+  const projectId = payload.project.projectId.selected_option.value
+
+  const projectRef = firestore.collection(`projects`).doc(projectId)
+  const project = await projectRef.get()
+  const { title } = project.data() as Project
+  // firestoreからプロジェクトとサブコレクションのチャレンジを削除
+  const batch = firestore.batch()
+  const challengesRef = await projectRef.collection(`challenges`).get()
+  challengesRef.docs.forEach(challenge => {
+    batch.delete(challenge.ref)
+  })
+  batch.delete(projectRef)
+  await batch.commit()
+
+  return {
+    token: context.botToken,
+    text: `新規プロジェクト[${title}]を削除しました`,
+    channel: metadata,
+    user,
+  }
+}
 
 export default function() {
   app.view(`cem_delete`, async ({ ack, body, view, context }) => {
     ack()
 
-    const payload = view.state.values
-    const user = body.user.id
-    const metadata = body.view.private_metadata
-    const projectId = payload.project.projectId.selected_option.value
-
-    const projectRef = firestore.collection(`projects`).doc(projectId)
-    const project = await projectRef.get()
-    const projData = project.data()
-    const projectTitle = projData!.title
-    // firestoreからプロジェクトとサブコレクションのチャレンジを削除
-    const batch = firestore.batch()
-    const challengesRef = await projectRef.collection(`challenges`).get()
-    challengesRef.docs.forEach(challenge => {
-      batch.delete(challenge.ref)
-    })
-    batch.delete(projectRef)
-    await batch.commit()
-    // 成功をSlack通知
-    const msg: Message = {
-      token: context.botToken,
-      text: `新規プロジェクト[${projectTitle}]を削除しました`,
-      channel: metadata,
-      user: user,
-    }
-    await app.client.chat.postEphemeral(msg).catch(err => {
+    const result = await deleteProject({ view, body, context })
+    await app.client.chat.postEphemeral(result).catch(err => {
       throw new Error(err)
     })
   })
